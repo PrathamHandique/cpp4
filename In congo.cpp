27f1bdace2b107dd0c8e461ff8bb3f408f3286b1
PrathamHandique <104254619+PrathamHandique@users.noreply.github.com>
2023-01-08 19:33:54 +0530

    Add files via upload

---
+++ In congo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int ageofcitizen;
+ cin>>ageofcitizen;
+ if(ageofcitizen>18&&ageofcitizen<70)
+ {cout<<"Eligible for Voting";}
+ else
+ {cout<<"Not Eligible for Voting";}
+return 0;
+}
